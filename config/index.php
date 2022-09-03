0a322322920e1ffd5cf7cfaa141bf987a70f7b21
yasiw <90653146+yasiiw@users.noreply.github.com>
2022-09-03 12:31:47 +0300

    Update index.php

--- config/index.php
+++ config/index.php
@@ -14,7 +14,7 @@ const DB_NAME = 'mai';
 /**
  * Github Client
  */
-const GITHUB_CLIENT_ID = "cd56ae8a18c5a7c6f8f1";
-const GITHUB_CLIENT_SECRET = "8ed897f31ec3d20133ca92beba57bee1c2364242";
+const GITHUB_CLIENT_ID = "your_client_id";
+const GITHUB_CLIENT_SECRET = "your_client_secret";
 const GITHUB_CLIENT_CALLBACK_URI = BASE_PATH."github-callback.php";
 
